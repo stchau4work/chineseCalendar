d0eb558fb6f0b5174f480e5591d5be93ff0d4ed5
Steven <stchau4work@gmail.com>
2017-11-05 18:22:38 +0800

    implemented the HK CNY methods

--- chineseCalendar.R
+++ chineseCalendar.R
@@ -1,5 +1,12 @@
+############################
+# Get Chinese New Year from 1901 to 2100
+# Reimplement from Dr. Herong Yang  http://www.herongyang.com/year/Program-Chinese-Calendar-Program-in-Java.html
+# Author: Steven Chau 
+# Date: 2017-11-05
+###########################
+
 rm(list=ls())
-library(bitops)
+lapply(c("bitops","data.table"), require, character.only = TRUE)
 
 # Base date: 01-Jan-1901, 4598/11/11 in Chinese calendar
 baseYear <- 1901
@@ -10,6 +17,10 @@ baseChineseYear <- 4598-1
 baseChineseMonth <- 11
 baseChineseDate <- 11
 
+#Chinese month map, 2 bytes per year, from 1900 to 2100, 402 bytes.
+#The first 4 bits represents the leap month of the year.
+#The rest 12 bits are flags indicate if the corresponding month
+#is a 29-day month. 2 bytes are stored in low-high order.
 chineseMonths<-c(
   0x00,0x04,0xad,0x08,0x5a,0x01,0xd5,0x54,0xb4,0x09,0x64,0x05,0x59,0x45,
   0x95,0x0a,0xa6,0x04,0x55,0x24,0xad,0x08,0x5a,0x62,0xda,0x04,0xb4,0x05,
@@ -88,25 +99,21 @@ daysInChineseMonth <- function(y,m) {
     v <- chineseMonths[2*index+1]
     l <- m - 1
     if (bitAnd((bitShiftR(v,l)),0x01)==1 ) d <- 29
-    print(paste0(d,'a'))
   } else if (9<=m && m<=12) {
     v <- chineseMonths[2*index+1+1]
     l <- m - 9
     if ( bitAnd((bitShiftR(v,l)),0x01)==1 ) d <- 29
-    print(paste0(d,'b'))
   } else { # leap month
     v <- chineseMonths[2*index+1+1]
     v <- bitAnd((bitShiftR(v,4)),0x0F)
     if (v!=abs(m)) {
       d <- 0 # wrong m specified
-      print(paste0(d,'c'))
     } else {
       d <- 29
       for (i in (1:length(bigLeapMonthYears))) {
         if (bigLeapMonthYears[i]==index) {
           d <- 30
           break
-          print(paste0(d,'d'))
         }
       }
     }
@@ -114,9 +121,13 @@ daysInChineseMonth <- function(y,m) {
   return(d)
 }
 
-compute.chinesefield <- function(gregorianYear,gregorianMonth,gregorianDate){
-  if(gregorianYear<1901 || gregorianYear>2100)
-    return(1)
+getChinesCalendarDate <- function(date){
+  try(if(!is.double(date)) stop("invalid date input"))
+  gregorianYear <- as.numeric(substr(as.character(date),0,4))
+  gregorianMonth <- as.numeric(substr(as.character(date),6,7))
+  gregorianDate <- as.numeric(substr(as.character(date),9,10))
+  if(gregorianYear<1901 || gregorianYear>2100) 
+    stop("date must within 1901 and 2100")
   startYear <- baseYear
   startMonth <- baseMonth
   startDate <- baseDate
@@ -127,7 +138,7 @@ compute.chinesefield <- function(gregorianYear,gregorianMonth,gregorianDate){
   #second base date: 01-Jan-2000, 4697/11/25 in Chinese calendar
   if(gregorianYear>=2000){
     startYear <- baseYear + 99
-    startMonth <- 2
+    startMonth <- 1
     startDate <- 1
     chineseYear <- baseChineseYear +99
     chineseMonth <- 11
@@ -139,15 +150,17 @@ compute.chinesefield <- function(gregorianYear,gregorianMonth,gregorianDate){
   #    for startMonth = 1 and startDate = 1
   daysDiff <- 0
   if(gregorianYear>startYear){
-  for (i in (startYear:(gregorianYear-1))){
-    daysDiff %+=% 365
-    if(isGeogorianLeapYear(i))
-      daysDiff %+=% 1
-  }}
+    for (i in (startYear:(gregorianYear-1))){
+      daysDiff %+=% 365
+      if(isGeogorianLeapYear(i))
+        daysDiff %+=% 1
+    }
+   }
   if(gregorianMonth>startMonth){
-  for (i in (startMonth:(gregorianMonth-1))){
-    daysDiff %+=% daysInGregorianMonth(gregorianYear,i)
-  }}
+    for (i in (startMonth:(gregorianMonth-1))){
+      daysDiff %+=% daysInGregorianMonth(gregorianYear,i)
+    }
+  }
   daysDiff %+=% (gregorianDate-startDate)
   # Adding that number of days to the Chinese date
   # Then bring Chinese date into the correct range.
@@ -159,17 +172,87 @@ compute.chinesefield <- function(gregorianYear,gregorianMonth,gregorianDate){
     if (abs(nextMonth)<abs(chineseMonth)) chineseYear %+=% 1
     chineseMonth <- nextMonth
     chineseDate %-=% lastDate
-    print(chineseMonth)
-    print(chineseYear)
-    
     lastDate <- daysInChineseMonth(chineseYear, chineseMonth)
     nextMonth <- nextChineseMonth(chineseYear, chineseMonth)
   }
-  ans <- paste0(chineseYear,'/',chineseMonth,'/',chineseDate)
-  return(ans)
+  calendar <- paste0(chineseYear,'-',chineseMonth,'-',chineseDate)
+  return(calendar)
 }
 
-#should return the first Chinese New Year in 2017
-compute.chinesefield(2017,1,29)
+# return the CNY for a given gregorian year
+getChineseNewYear <- function(year){
+  t <-c()
+  firstDay <- as.Date(paste0(as.character(year),"-01-01"))
+  t$gregorian <- seq(firstDay, length.out=61, by='day')
+  #search for the first 61 days for the CNY
+  for(i in t$g){
+    d <- getChinesCalendarDate(as.Date(i, origin="1970-01-01"))
+    t$chinese <- c(t$chinese,d)
+  }
+  t <- as.data.table(t)
+  t[,cyear:=as.numeric(substr(chinese,0,4))]
+  t[,cyear_lag:=shift(cyear,1,type="lag")]
+  t[!is.na(cyear_lag),diff:=cyear-cyear_lag]
+  as.character(t[diff>0]$gregorian)
+}
+
+# return CNY for multiple gregorian years
+getChineseNewYears <- function(from,to){
+  cny<-c()
+  while(from<=to){
+    cny <- c(cny,getChineseNewYear(from))
+    from %+=% 1
+  }
+  cny
+}
+
+# return a list of HK CNY holidays with a given gregorian year
+getHKChineseNewYearHolidays <- function(year){
+  d<-as.Date(getChineseNewYear(year))
+  if(weekdays(d) %in% c("Friday","Saturday","Sunday"))
+    seq(d, length.out=4, by='day')
+  else
+    seq(d, length.out=3, by='day')
+}
+
+
+##############################################################
+# http://www.chinesenewyears.info/chinese-new-year-calendar.php
+# Testing getChineseNewYear 
+getChineseNewYear(2000) == as.Date("2000-02-05")
+getChineseNewYear(2001) == as.Date("2001-01-24")
+getChineseNewYear(2002) == as.Date("2002-02-12")
+getChineseNewYear(2003) == as.Date("2003-02-01")
+getChineseNewYear(2004) == as.Date("2004-01-22")
+getChineseNewYear(2005) == as.Date("2005-02-09")
+getChineseNewYear(2006) == as.Date("2006-01-29")
+getChineseNewYear(2007) == as.Date("2007-02-18")
+getChineseNewYear(2008) == as.Date("2008-02-07")
+getChineseNewYear(2009) == as.Date("2009-01-26")
+getChineseNewYear(2010) == as.Date("2010-02-14")
+getChineseNewYear(2011) == as.Date("2011-02-03")
+getChineseNewYear(2012) == as.Date("2012-01-23")
+getChineseNewYear(2013) == as.Date("2013-02-10")
+getChineseNewYear(2014) == as.Date("2014-01-31")
+getChineseNewYear(2015) == as.Date("2015-02-19")
+getChineseNewYear(2016) == as.Date("2016-02-08")
+getChineseNewYear(2017) == as.Date("2017-01-28")
+getChineseNewYear(2018) == as.Date("2018-02-16")
+getChineseNewYear(2019) == as.Date("2019-02-05")
+getChineseNewYear(2020) == as.Date("2020-01-25")
+
+# Testing getChineseNewYears 
+getChineseNewYears(2020,2025) == c(as.Date("2020-01-25"),as.Date("2021-02-12"),as.Date("2022-02-01"),as.Date("2023-01-22"),as.Date("2024-02-10"),as.Date("2025-01-29"))
+
+# Testing HK Chinese New Year Holidays
+# https://www.timeanddate.com/calendar/?year=2017&country=42
+getHKChineseNewYearHolidays(2017) ==c(as.Date("2017-1-28"),as.Date("2017-1-29"),as.Date("2017-1-30"),as.Date("2017-1-31"))
+getHKChineseNewYearHolidays(2018) ==c(as.Date("2018-2-16"),as.Date("2018-2-17"),as.Date("2018-2-18"),as.Date("2018-2-19"))
+getHKChineseNewYearHolidays(2019) ==c(as.Date("2019-2-5"),as.Date("2019-2-6"),as.Date("2019-2-7"))
+getHKChineseNewYearHolidays(2020) ==c(as.Date("2020-1-25"),as.Date("2020-1-26"),as.Date("2020-1-27"),as.Date("2020-1-28"))
+getHKChineseNewYearHolidays(2021) ==c(as.Date("2021-2-12"),as.Date("2021-2-13"),as.Date("2021-2-14"),as.Date("2021-2-15"))
+
+
+
 
 
